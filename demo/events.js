1e28ebcf358667ecfccde0e1ea3f4b7ac2ac6407
belartale <belartale@gmail.com>
2021-01-08 22:30:16 +0200

    save

--- demo/events.js
+++ demo/events.js
@@ -2,8 +2,24 @@ const EventEmitter = require("events");
 
 const emitter = new EventEmitter();
 
-emitter.on("sobutia", (data) => {
-  console.log("on sobutia", data);
-});
+// emitter.on("sobutia", (data) => {
+//   console.log("on sobutia", data);
+// });
 
-emitter.emit("sobutia", { a: 3 });
+// emitter.emit("sobutia", { a: 3 });
+
+// setTimeout(() => {
+//   emitter.emit("sobutia", { b: 31 });
+// }, 2000);
+
+class Dispatcher extends EventEmitter {
+  subscride(eventName, cd) {
+    console.log("[Subscride ...]");
+    this.on(eventName, cd);
+  }
+
+  dispatch(eventName, data) {
+    console.log("[Dispatch ...]");
+    this.emit(eventName, data);
+  }
+}
