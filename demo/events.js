1609ea4de086124e0a213d37f63c973b10ef6ccb
belartale <belartale@gmail.com>
2021-01-16 21:14:37 +0200

    save

--- demo/events.js
+++ demo/events.js
@@ -2,32 +2,39 @@ const EventEmitter = require("events");
 
 const emitter = new EventEmitter();
 
+//! .on проглушивать события
 // emitter.on("sobutia", (data) => {
 //   console.log("on sobutia", data);
 // });
 
+//! по нужно имитить собития .emit
 // emitter.emit("sobutia", { a: 3 });
 
+// можно сделать так
 // setTimeout(() => {
 //   emitter.emit("sobutia", { b: 31 });
 // }, 2000);
 
+// можем наследоваться от слассов
 class Dispatcher extends EventEmitter {
   subscribe(eventName, cd) {
-    console.log("[Subscride ...]");
+    console.log("[Subscribe ...]");
     this.on(eventName, cd);
   }
 
   dispatch(eventName, data) {
-    console.log("[Dispatch ...]");
+    console.log("[Dispatching ...]");
     this.emit(eventName, data);
   }
 }
 
 const dis = new Dispatcher();
 
+// диспатчить нужно после проглушки
+//dis.dispatch("aa", { aa: 22 });
+
 dis.subscribe("aa", (data) => {
-  console.log("ON:", data);
+  console.log("ON: aa", data);
 });
 
 dis.dispatch("aa", { aa: 22 });
