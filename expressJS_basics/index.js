a260b86fc9473597ccff998b79e1e73e0af1c8ae
belartale <belartale@gmail.com>
2021-02-21 17:03:04 +0200

    save

--- expressJS_basics/index.js
+++ expressJS_basics/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
 const app = express(); // app эти типа объекта и инициализация
 
 app.get("/", (req, res) => {
-  res.send("<h1>exp!!!!11</h1>");
+  // res.send("<h1>exp</h1>");
+  res.sendFile(path.resolve(__dirname, "static", "index.html"));
+});
+
+app.get("/features", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "static", "features.html"));
+});
+
+app.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname, "static", "index.html"));
 });
 
 app.listen(PORT, () => {
