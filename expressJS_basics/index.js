07a27f7f23b6e6563adccd44ce321ebc83fe9e61
Arthur Belan <belartale@gmail.com>
2021-02-24 00:22:18 +0200

    ejs

--- expressJS_basics/index.js
+++ expressJS_basics/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { Agent } from "http";
 import path from "path";
 import { requestTime, logger } from "./mibblewares.js";
 
@@ -6,9 +7,21 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
 const app = express(); // app эти типа объекта и инициализация
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs"));
+// console.log(app.get("views"));
+
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use(requestTime);
 app.use(logger);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Main page" });
+});
+
+app.get("/fea", (req, res) => {
+  res.render("fea", { title: "FEA" });
+});
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "static", "index.html"));
