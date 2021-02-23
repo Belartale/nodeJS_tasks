41638ac3f39c55dcf3b51a5821dabb88229ca135
Arthur Belan <belartale@gmail.com>
2021-02-23 20:42:54 +0200

    save Express

--- expressJS_basics/index.js
+++ expressJS_basics/index.js
@@ -1,22 +1,27 @@
 import express from "express";
 import path from "path";
+import { requestTime, logger } from "./mibblewares.js";
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
 const app = express(); // app эти типа объекта и инициализация
 
-app.get("/", (req, res) => {
-  // res.send("<h1>exp</h1>");
-  res.sendFile(path.resolve(__dirname, "static", "index.html"));
-});
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(requestTime);
+app.use(logger);
 
-app.get("/features", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "static", "features.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "static", "index.html"));
+// });
 
-app.get("/download", (req, res) => {
-  res.download(path.resolve(__dirname, "static", "index.html"));
-});
+// app.get("/features", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "static", "features.html"));
+// });
+
+// app.get("/download", (req, res) => {
+//   console.log(req.requestTime);
+//   res.download(path.resolve(__dirname, "static", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`server start ${PORT}!!! ...`);
