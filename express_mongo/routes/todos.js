491aa4a782d6a3b4d5e73119f8849bba59a7cdfa
belartale <belartale@gmail.com>
2021-04-16 17:51:29 +0300

    save

--- express_mongo/routes/todos.js
+++ express_mongo/routes/todos.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
+const Todo = require("../models/Todo");
+
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const todos = await Todo.find({});
+
   res.render("index", {
     title: "index t",
     isIndex: true,
+    todos,
   });
 });
 
