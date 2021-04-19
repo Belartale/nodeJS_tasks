6c1c00d6a437653f817dcc3414866b6ad6136307
belartale <belartale@gmail.com>
2021-04-19 15:37:49 +0300

    add css

--- express_mongo/routes/todos.js
+++ express_mongo/routes/todos.js
@@ -3,28 +3,25 @@ const Todo = require("../models/Todo");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const todos = await Todo.find({});
+  const todos = await Todo.find({}).lean();
 
-  let arr = [];
-
-  let set = new Set();
-
-  await todos.forEach((object) => {
-    for (const key in object) {
-      if (key == "title") {
-        const element = object[key];
-        arr.push(element);
-      }
-    }
-  });
+  // await let arr = [];
+  // await todos.forEach((object) => {
+  //   for (const key in object) {
+  //     if (key == "title") {
+  //       const element = object[key];
+  //       arr.push(element);
+  //     }
+  //   }
+  // });
 
   await res.render("index", {
     title: "INDEX t",
     isIndex: true,
-    todos: arr,
+    todos: todos,
   });
 
-  await console.log(arr);
+  // await console.log(arr);
 });
 
 router.get("/create", (req, res) => {
